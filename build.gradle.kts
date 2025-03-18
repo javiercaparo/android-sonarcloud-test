78c6301c8bc9ede6e0e60749eb5f459e1d88b7ea
Javier Caparo <55814804+javier-caparo@users.noreply.github.com>
2025-03-18 10:30:16 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     id("org.sonarqube") version "6.0.1.5171"
+    id "org.jlleitschuh.gradle.ktlint" version "7.1.0"
 }
 
 version = "1.0-SNAPSHOT"
